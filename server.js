1d50489ed69cb370609114e8d0e719c139360856
kmelter <kmelter97@gmail.com>
2022-04-23 12:34:28 -0500

    install uuid

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-const termData = require('./terms.json');
-const PORT = 3001;
-
-// Initialize our app variable by setting it to the value of express()
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { db } = require('./db/db');
+const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const res = require('express/lib/response');
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.use(express.static('public'));
 
-// Add a static route for index.html
 app.get('/', (req, res) => {
-  // `res.sendFile` is Express' way of sending a file
-  // `__dirname` is a variable that always returns the directory that your server is running in
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = db.length.toString();
+
+  //TODO  
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
