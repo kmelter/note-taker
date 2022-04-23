e3913553ec91e914e45c0c83ee35f46faf9c5b41
kmelter <kmelter97@gmail.com>
2022-04-23 14:46:56 -0500

    add note functionality

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const uuid = require('./helpers/uuid');
 const res = require('express/lib/response');
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+// function createNewNote(body, notesArray) {
+//   const note = body;
+//   notesArray.push(note);
+//   fs.writeFileSync(
+//     path.join(__dirname, './db/db.json'),
+//     JSON.stringify({ notes: notesArray }, null, 2)
+//   );
+//   return note;
+// }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
@@ -26,10 +37,48 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  req.body.id = db.length.toString();
+  // req.body.id = db.length.toString();
+  console.info(`${req.method} request recieved to add a note`);
 
-  //TODO  
-})
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+
+        parsedNotes.push(newNote);
+
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) => 
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  } 
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
